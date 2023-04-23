e758878a4497ef69550aaf70747229237541a2f0
zakzakst <e1433104@gmail.com>
2023-04-24 02:46:14 +0900

    Updates

--- assets/config-3586b156.js
+++
@@ -1,2 +0,0 @@
-import{d as t,r as m,a as s}from"./decorateStory-1edec423.js";import"./iframe-acc25ba2.js";import"./vue.esm-bundler-6ef4fa0f.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
-//# sourceMappingURL=config-3586b156.js.map
---
+++ assets/config-d271e64c.js
@@ -0,0 +1,2 @@
+import{d as t,r as m,a as s}from"./decorateStory-76cf0079.js";import"./iframe-c98ed0f8.js";import"./vue.esm-bundler-6ef4fa0f.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
+//# sourceMappingURL=config-d271e64c.js.map
