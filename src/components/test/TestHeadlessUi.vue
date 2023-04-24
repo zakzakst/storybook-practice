5669061b0fec63ddaaca5682ed3ae507a6bc5f72
zakzakst <e1433104@gmail.com>
2023-04-24 15:01:09 +0900

    修正

--- src/components/test/TestHeadlessUi.vue
+++ src/components/test/TestHeadlessUi.vue
@@ -36,6 +36,22 @@
         </ListboxOptions>
       </Listbox>
     </div>
+
+    <div>
+      <Listbox v-model="selectedPerson">
+        <ListboxButton>{{ selectedPerson.name }}</ListboxButton>
+        <ListboxOptions>
+          <ListboxOption
+            v-for="person in people"
+            :key="person.id"
+            :value="person"
+            :disabled="person.unavailable"
+          >
+            {{ person.name }}
+          </ListboxOption>
+        </ListboxOptions>
+      </Listbox>
+    </div>
   </div>
 </template>
 
